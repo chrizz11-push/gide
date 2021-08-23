2c11045e5a78221f66f023b9d3d0475582bd6ae8
chrizz11-push <christopherchima11@gmail.com>
2021-08-23 15:58:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.get("/", (req,res) => {
+    res.status("api connected")
+})
+
+app.listen(port, () => {
+    console.log(`server is listening to:${port}`)
+})
